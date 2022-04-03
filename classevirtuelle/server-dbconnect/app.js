32d1ba527e0b0379a74c709a90b91139a44e8e45
christellengamga <christellengamga@yahoo.fr>
2022-04-03 18:11:24 -0400

    ajout de socket.io partie server

--- classevirtuelle/server-dbconnect/app.js
+++ classevirtuelle/server-dbconnect/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 
+
+//test socket.io
+// Chargement de la page index.html
+app.get('/', function (req, res) {
+	console.log('ROUTE GET');
+  res.sendFile(__dirname + '/index.html');
+   
+});
+
+// Chargement de la page client.html
+app.get('/client', function (req, res) {
+  res.sendFile(__dirname + '/client.html');
+});  //fin tests pour socket.io
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Bring in the routes
+
+/*const userRoutes = require('./routes/user');
+
 //app.use("/user", require("./routes/user"));
+  app.use('/api/user',userRoutes);  */
 
 // Setup Error Handlers
 const errorHandlers = require("../handlers/errorHandlers");
@@ -17,5 +36,7 @@ if (process.env.ENV === "DEVELOPMENT") {
 } else {
   app.use(errorHandlers.productionErrors);
 }
+
+
 
 module.exports = app;
--- classevirtuelle/server-dbconnect/server.js
+++ classevirtuelle/server-dbconnect/server.js
@@ -19,6 +19,44 @@ mongoose.connection.once("open", () => {
 
 const app = require("./app");
 
-app.listen(8000, () => {
-  console.log("Server listenting on port 8000");
+//intégration socket.io
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");  
+const httpServer = createServer(app);
+const io = new Server(httpServer, {  });  
+const ent = require('ent');
+
+
+  io.on("connection", (socket) => {
+	  console.log('connexion serveur');
+      console.log(socket.id);
+	 
+    socket.on('nouvel_utilisateur', function(pseudo) {
+	  console.log('New user : '+pseudo);
+      pseudo = ent.encode(pseudo);
+      socket.pseudo = pseudo; 
+      socket.broadcast.emit('nouvel_utilisateur', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+	  console.log('message : ');
+      message = ent.encode(message);
+      console.log(message);
+	console.log('de l\'utilisateur');
+      socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
+  
+    socket.on('déconnexion', function(pseudo) {
+    pseudo = ent.encode(pseudo);
+    socket.pseudo = pseudo;
+    socket.broadcast.emit(pseudo, 'déconnecté');
+    });	
+	  
+ });
+
+//app.listen a été remplacé par httpServer.listen (comme indiqué sur le site socket.io)
+httpServer.listen(8080, () => {
+  console.log("Server listenting on port 8080");
 });
