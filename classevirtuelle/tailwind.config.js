4bf30ff4f024171d5ed169df23772ddca6c54d49
mathrob <mathrob22@hotmail.com>
2022-04-04 19:11:32 -0400

    tailwindcss update v2 to v3 config

--- classevirtuelle/tailwind.config.js
+++ classevirtuelle/tailwind.config.js
@@ -1,24 +1,19 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-       gb: '#333333',
-       gg: '#4d4d4d', 
+        gb: "#333333",
+        gg: "#4d4d4d",
       },
-
       fontFamily: {
-        logo: [ "Audiowide", "sans-serif" ],
-        text: [ "Quantico", "sans-serif" ],
+        logo: ["Audiowide", "sans-serif"],
+        text: ["Quantico", "sans-serif"],
+      },
     },
-
-   },
-
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
-}
+};
