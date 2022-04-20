10febb4e7cc0e3895cd0808b729e79b4a5383d56
mathrob <mathrob22@hotmail.com>
2022-04-20 13:06:35 -0400

    insert socket dans chat.js

--- classevirtuelle/pages/chat.js
+++ classevirtuelle/pages/chat.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { getSession } from "next-auth/client";
 
-function chat() {
-  return <div>Mon Super Chat!!!!</div>;
-}
+import { io } from "socket.io-client";
 
-export default chat;
+export default function chat() {
+  const [message, setMessage] = useState("");
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    setSocket(io());
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(message);
+    socket.emit("message", { message });
+    setMessage("");
+  };
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          placeholder="Type message"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          required
+        ></input>
+        <button type="submit">Envoye</button>
+      </form>
+    </div>
+  );
+}
 
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
