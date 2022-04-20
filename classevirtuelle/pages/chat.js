c01c79ad926bdb2831af54369732d56e2321da01
mathrob <mathrob22@hotmail.com>
2022-04-20 13:30:27 -0400

    acces au info user dans chat.js

--- classevirtuelle/pages/chat.js
+++ classevirtuelle/pages/chat.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 import { getSession } from "next-auth/client";
+import { loadUser } from "../redux/actions/userActions";
+import { useDispatch, useSelector } from "react-redux";
 
 import { io } from "socket.io-client";
 
-export default function chat() {
+export default function chat({ session }) {
+  const dispatch = useDispatch();
+
   const [message, setMessage] = useState("");
   const [socket, setSocket] = useState();
+
+  const { user, loading } = useSelector((state) => state.loadedUser);
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(loadUser());
+    }
+  }, [dispatch, user]);
 
   useEffect(() => {
     setSocket(io());
@@ -18,8 +30,10 @@ export default function chat() {
     socket.emit("message", { message });
     setMessage("");
   };
+
   return (
     <div className="App">
+      <div>{user && `Connecter : ${user.name}`}</div>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -50,6 +64,8 @@ export async function getServerSideProps(context) {
   }
 
   return {
-    props: {},
+    props: {
+      session,
+    },
   };
 }
